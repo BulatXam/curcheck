21fb6908074eea4c58422ab738b1b212078d28d6
BulatXam <Khamdbulat@yandex.ru>
2023-09-08 22:18:39 +0300

    feat: add inheritance

--- curcheck/events.py
+++ curcheck/events.py
@@ -8,18 +8,27 @@ import asyncio
 from aiohttp import ClientSession
 from lxml import html
 
-from typing import Any, List, Awaitable
+from typing import List, Awaitable
 
 from pyppeteer.browser import Browser
+
+from .errors import ConfigurationError
 
 
 class AbstractEvent:
     task: Awaitable|None = None
 
-    def __init__(self, domain: str, url: str, is_browser: bool = False):
+    def __init__(
+        self, 
+        domain: str, 
+        url: str, 
+        is_browser: bool = False, 
+        cookies: dict|None = None
+    ):
         self.domain = domain
         self.url = url
         self.is_browser = is_browser
+        self.cookies = cookies
 
         if "http://" in self.url and "." in self.url:
             self.link: str = self.url
@@ -27,26 +36,44 @@ class AbstractEvent:
             self.link: str = "http:" + self.url
         else:
             self.link: str = self.domain + self.url
+    
+    async def _get_page(self, browser: Browser):
+        page = await browser.newPage()
+        page.setDefaultNavigationTimeout(0)
+        if self.cookies:
+            await page.setCookie(*self.cookies)
+        await page.goto(url=self.link)
 
 
 class EventPage(AbstractEvent):
-    def __init__(self, domain: str, url: str, is_browser: bool = False):
-        super().__init__(domain=domain, url=url, is_browser=is_browser)
+    def __init__(
+        self, 
+        domain: str, 
+        url: str, 
+        is_browser: bool = False, 
+        cookies: dict|None = None
+    ):
+        super().__init__(
+            domain=domain, url=url, is_browser=is_browser, cookies=cookies
+        )
 
-    def __call__(self, func):
+    def __call__(self, func: Awaitable):
         @functools.wraps(func) 
-        async def spa_task(browser: Browser):
+        async def spa_task(browser: Browser, *args, **kwargs):
             page = await browser.newPage()
+            page.setDefaultNavigationTimeout(0)
+            if self.cookies:
+                await page.setCookie(*self.cookies)
             await page.goto(url=self.link)
-            await func(page)
+            await func(page, *args, **kwargs)
             await page.close()
 
         @functools.wraps(func) 
-        async def mpa_task(session: ClientSession):
+        async def mpa_task(session: ClientSession, *args, **kwargs):
             response = await session.request("GET", self.link)
             tree = html.fromstring(await response.text())
 
-            await func(tree)
+            await func(tree, *args, **kwargs)
 
         if self.is_browser:
             self.task = spa_task
@@ -59,45 +86,74 @@ class EventPage(AbstractEvent):
 class EventPaginator(AbstractEvent):
     def __init__(
         self, 
-        domain: str, 
-        url: str, 
-        pages_links_xpath: str, 
+        domain: str|None = None, 
+        url: str|None = None, 
+        pages_links_xpath: str|None = None,
         count_in_approach: int = 10,
-        is_browser: bool = False
+        is_browser: bool = False,
+        auxiliary_function: Awaitable|None = None,
+        paginate_urls: list|None = None,
+        cookies: dict|None = None
     ) -> None:
-        super().__init__(domain=domain, url=url, is_browser=is_browser)
+        super().__init__(
+            domain=domain, url=url, is_browser=is_browser, cookies=cookies
+        )
 
-        self.pages_links_xpath = pages_links_xpath
-        self.count_in_approach = count_in_approach
+        if domain and url:
+            self.pages_links_xpath = pages_links_xpath
+            self.count_in_approach = count_in_approach
+            self.auxiliary_function = auxiliary_function
+        elif paginate_urls:
+            self.paginate_urls = paginate_urls
+        else:
+            raise ConfigurationError(
+                "Ошибка! Нельзя одновременно парсить пагинатор страницы и "\
+                "отправлять свои ссылки для пагинации!"
+            )
+
 
         self.pages: List[EventPage] = []
 
-    def __call__(self, func):
+    def __call__(self, func: Awaitable):
         @functools.wraps(func) 
-        async def spa_task(browser: Browser):
-            base_page = await browser.newPage()
-            await base_page.goto(url=self.domain+self.url)
-            
-            if self.pages_links_xpath.split("/")[-1][0] == "@":
-                html_attr = f'.getAttribute("{self.pages_links_xpath.split("/")[-1][1:]}")'
-            elif self.pages_links_xpath.split("/")[-1] == "text()":
-                html_attr = ".innerText"
+        async def spa_task(browser: Browser, *args, **kwargs):
+            if not self.paginate_urls:
+                base_page = await browser.newPage()
+                base_page.setDefaultNavigationTimeout(0)
+                if self.cookies:
+                    await base_page.setCookie(*self.cookies)
+                await base_page.goto(url=self.domain+self.url)
+
+                if self.auxiliary_function:
+                    await self.auxiliary_function(base_page)
+                
+                if self.pages_links_xpath.split("/")[-1][0] == "@":
+                    html_attr = f'.getAttribute("{self.pages_links_xpath.split("/")[-1][1:]}")'
+                elif self.pages_links_xpath.split("/")[-1] == "text()":
+                    html_attr = ".innerText"
+                else:
+                    html_attr = ".innerHTML"
+
+                xpath = "/".join(self.pages_links_xpath.split("/")[:-1])
+
+                pages_links = await base_page.xpath(xpath)
+
+                self.pages = [
+                    EventPage(
+                        domain=self.domain, 
+                        url=await base_page.evaluate(
+                            f"(link) => link{html_attr}", link
+                        ),
+                        is_browser=True,
+                        cookies=self.cookies
+                    )(func) for link in pages_links
+                ]
             else:
-                html_attr = ".innerHTML"
-
-            xpath = "/".join(self.pages_links_xpath.split("/")[:-1])
-
-            pages_links = await base_page.xpath(xpath)
-
-            self.pages = [
-                EventPage(
-                    domain=self.domain, 
-                    url=await base_page.evaluate(
-                        f"(link) => link{html_attr}", link
-                    ),
-                    is_browser=True
-                )(func) for link in pages_links
-            ]
+                self.pages = [
+                    EventPage(
+                        domain=self.domain, url=url, is_browser=True
+                    )(func) for url in self.paginate_urls
+                ]
 
             approach_len = (
                 len(self.pages) // self.count_in_approach 
@@ -108,18 +164,22 @@ class EventPaginator(AbstractEvent):
             for i in range(approach_len):
                 if i < approach_len - 1:
                     await asyncio.gather(
-                        *[page_task(browser) for page_task in self.pages[i: (i+1)*self.count_in_approach]]
+                        *[page_task(browser, *args, **kwargs) for page_task in self.pages[i: (i+1)*self.count_in_approach]]
                     )
                 else:
                     await asyncio.gather(
-                        *[page_task(browser) for page_task in self.pages[i: len(self.pages)-i*self.count_in_approach]]
+                        *[page_task(browser, *args, **kwargs) for page_task in self.pages[i: len(self.pages)-i*self.count_in_approach]]
                     )
 
         @functools.wraps(func) 
-        async def mpa_task(session: ClientSession):
+        async def mpa_task(session: ClientSession, *args, **kwargs):
             response = await session.request("GET", self.link)
 
             base_tree = html.fromstring(await response.text())
+
+            if self.auxiliary_function:
+                await self.auxiliary_function(base_tree)
+
             pages_links_elements = base_tree.xpath(self.pages_links_xpath)
 
             self.pages = [
@@ -139,11 +199,11 @@ class EventPaginator(AbstractEvent):
             for i in range(approach_len):
                 if i < approach_len - 1:
                     await asyncio.gather(
-                        *[page_task(session) for page_task in self.pages[i: (i+1)*self.count_in_approach]]
+                        *[page_task(session, *args, **kwargs) for page_task in self.pages[i: (i+1)*self.count_in_approach]]
                     )
                 else:
                     await asyncio.gather(
-                        *[page_task(session) for page_task in self.pages[i: len(self.pages)-i*self.count_in_approach]]
+                        *[page_task(session, *args, **kwargs) for page_task in self.pages[i: len(self.pages)-i*self.count_in_approach]]
                     )
 
         if self.is_browser:
@@ -161,33 +221,37 @@ class EventLongpoll(AbstractEvent):
         url: str, 
         timeout: int = 60, 
         count: int = None,
-        is_browser: bool = False
+        is_browser: bool = False,
+        cookies: dict|None = None,
     ) -> None:
-        super().__init__(domain=domain, url=url, is_browser=is_browser)
+        super().__init__(domain=domain, url=url, is_browser=is_browser, cookies=cookies)
 
         self.count = count
         self.timeout = timeout
         self.i = 0
 
-    def __call__(self, func) -> Any:
+    def __call__(self, func: Awaitable):
         @functools.wraps(func)
-        async def spa_task(browser: Browser):
+        async def spa_task(browser: Browser, *args, **kwargs):
             self.browser = browser
             page = await self.browser.newPage()
+            if self.cookies:
+                await page.setCookie(*self.cookies)
+            page.setDefaultNavigationTimeout(0)
             await self.page.goto(self.link)
             while self.i <= self.count or self.count == None:
                 self.i += 1
-                await func(page)
+                await func(page, *args, **kwargs)
                 await page.reload()
                 await asyncio.sleep(self.timeout)
 
         @functools.wraps(func) 
-        async def mpa_task(session: ClientSession):
+        async def mpa_task(session: ClientSession, *args, **kwargs):
             while self.i <= self.count or self.count == None:
                 response = await session.request("GET", self.link)
                 
                 tree = html.fromstring(await response.text())
-                await func(tree)
+                await func(tree, *args, **kwargs)
 
                 await asyncio.sleep(self.timeout)
                 self.i += 1
